0cfeb8a90af8adfba253c99f278be71e54d3d5fc
Jeremy Maurer <jlmaurer@stanford.edu>
2018-05-25 11:44:12 -0700

    Update referencing and data structures to reflect new directories

--- NMR_codes/loadnmrdata2.m
+++ NMR_codes/loadnmrdata2.m
@@ -3,7 +3,7 @@ function [d, K, T2ML, phi, z, SumEch, log10K, log10T2, log10Porosity, SumEch_3s,
     % variables. 
     
     if strcmp(name, 'A1') ==0 && strcmp(name, 'C1') == 0 && strcmp(name, 'gems_all') ==0 && strcmp(name, 'all_data') == 0
-        in=([pwd filesep 'bstrp_dat_VC_processed' filesep name '.txt']);               %define the datafile name
+        in=(['..' filesep '..' filesep 'Data' filesep 'Aggregated_data' filesep name '.txt']);               %define the datafile name
         d=load(in);                         %load the raw data
         K= d(:,4);                %convert direct data to K in [m filesep s] 
         T2ML=d(:,2);                        %determine range of T2ML data, converts Vista Clara to [ms]
@@ -19,19 +19,28 @@ function [d, K, T2ML, phi, z, SumEch, log10K, log10T2, log10Porosity, SumEch_3s,
         log10Porosity = log10(phi);
         
     elseif strcmp(name, 'A1') ==1 
-        str = ['dpnmr_site_' name]; 
-        load(str)
+        str = ['..' filesep 'Data' filesep 'dpnmr_site_' name];
+        try
+            load(str)
+        catch
+            keyboard;
+        end
         T2ML = T2; 
         log10Porosity = log10(phi); 
         log10T2 = log10(T2ML);
         SumEch = 10.^SOE; 
         d = [z, T2ML, phi, K, SumEch];
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrA11.txt']; 
-        d2 = load(str2);
+        str2 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrA11.txt']; 
+        try
+            d2 = load(str2);
+        catch
+            keyboard; 
+        end
+        
         SumEch_3s1 = d2(:,6); 
         SumEch_twm1 = d2(:,7); 
         SumEch_twm_3s1 = d2(:,8); 
-        str3 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrA12.txt'];
+        str3 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrA12.txt'];
         d3 = load(str3);
         SumEch_3s = mean([SumEch_3s1, d3(:,6)], 2); 
         SumEch_twm = mean([SumEch_twm1, d3(:,7)], 2); 
@@ -39,24 +48,24 @@ function [d, K, T2ML, phi, z, SumEch, log10K, log10T2, log10Porosity, SumEch_3s,
         log10K = log10(K); 
         
     elseif strcmp(name, 'C1') == 1
-        str = ['dpnmr_site_' name]; 
+        str = ['..' filesep 'Data' filesep 'dpnmr_site_' name]; 
         load(str)
         T2ML = T2; 
         log10Porosity = log10(phi); 
         log10T2 = log10(T2ML);
         SumEch = 10.^SOE; 
         d = [z, T2ML, phi, K, SumEch];
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1S.txt']; 
+        str2 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrC1S.txt']; 
         d2 = load(str2);
         SumEch_3s1 = d2(:,6); 
         SumEch_twm1 = d2(:,7); 
         SumEch_twm_3s1 = d2(:,8); 
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1SW.txt']; 
+        str2 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrC1SW.txt']; 
         d2 = load(str2);
         SumEch_3s2 = d2(:,6); 
         SumEch_twm2 = d2(:,7); 
         SumEch_twm_3s2 = d2(:,8);
-        str3 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1SE.txt'];
+        str3 = ['..' filesep 'Data'  filesep 'Aggregated_data' filesep 'dpnmrC1SE.txt'];
         d3 = load(str3);
         SumEch_3s = mean([SumEch_3s1, SumEch_3s2, d3(:,6)], 2); 
         SumEch_twm = mean([SumEch_twm1, SumEch_twm2, d3(:,7)], 2); 
@@ -64,19 +73,19 @@ function [d, K, T2ML, phi, z, SumEch, log10K, log10T2, log10Porosity, SumEch_3s,
        
     elseif strcmp(name, 'gems_all') ==1
         
-        str = ['dpnmr_site_C1']; 
+        str = ['..' filesep 'Data' filesep 'dpnmr_site_C1']; 
         load(str)
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1S.txt']; 
+        str2 = ['..'  filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrC1S.txt']; 
         d2 = load(str2);
         SumEch_3s1 = d2(:,6); 
         SumEch_twm1 = d2(:,7); 
         SumEch_twm_3s1 = d2(:,8); 
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1SW.txt']; 
+        str2 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrC1SW.txt']; 
         d2 = load(str2);
         SumEch_3s2 = d2(:,6); 
         SumEch_twm2 = d2(:,7); 
         SumEch_twm_3s2 = d2(:,8);
-        str3 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrC1SE.txt'];
+        str3 = ['..' filesep 'Data'  filesep 'Aggregated_data' filesep 'dpnmrC1SE.txt'];
         d3 = load(str3);
         
         SumEch_3sC = mean([SumEch_3s1, SumEch_3s2, d3(:,6)], 2); 
@@ -90,16 +99,16 @@ function [d, K, T2ML, phi, z, SumEch, log10K, log10T2, log10Porosity, SumEch_3s,
         SumEchc = 10.^SOE; 
 
         clearvars -except zC T2C Dkc phic SumEchc SumEch_3sC SumEch_twmC SumEch_twm_3sC
-        str = ['dpnmr_site_A1']; 
+        str = ['Data' filesep 'dpnmr_site_A1']; 
         load(str)
         T2ML = T2; 
         SumEch = 10.^SOE; 
-        str2 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrA11.txt']; 
+        str2 = ['..' filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrA11.txt']; 
         d2 = load(str2);
         SumEch_3s1 = d2(:,6); 
         SumEch_twm1 = d2(:,7); 
         SumEch_twm_3s1 = d2(:,8); 
-        str3 = [pwd  filesep 'bstrp_dat_VC_processed' filesep 'dpnmrA12.txt'];
+        str3 = ['..'  filesep 'Data' filesep 'Aggregated_data' filesep 'dpnmrA12.txt'];
         d3 = load(str3);
         SumEch_3sA = mean([SumEch_3s1, d3(:,6)], 2); 
         SumEch_twmA = mean([SumEch_twm1, d3(:,7)], 2); 
